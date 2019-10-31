44b183a3451273a137888788ca7b8b0ab5a318ce
denise <denise.mai21@gmail.com>
2019-10-30 20:32:07 -0400

    first project commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { WatchListPageComponent } from './watch-list-page/watch-list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    MovieListComponent,
+    WatchListPageComponent
   ],
   imports: [
     BrowserModule
